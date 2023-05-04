40ac1d63ce305cd5a2ed626c4e26b1cde35b22c9
1312ds <129785596+1312ds@users.noreply.github.com>
2023-05-04 21:34:40 +0000

    test03

--- platformgame.java
+++ platformgame.java
@@ -5,6 +5,7 @@ public class platformgame {
     public static void main(String[] args) {
         System.out.println("hi 01");
         System.out.println("hi 02");
+        System.out.println("hi 03");
 
     }
 
