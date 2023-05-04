88bc1c9bc1864006392a45acf55bc360144060ce
Iknbany2 <131987262+Iknbany2@users.noreply.github.com>
2023-05-04 22:14:52 +0000

    saad-1-16

---
+++ src/main/Game.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * Game
+ */
+public class Game {
+    private GameWindow gameWindow;
+
+    public Game() {
+        gameWindow=new GameWindow();
+        
+    }
+}
---
+++ src/main/mainclass.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * mainclass
+ */
+public class mainclass {
+
+    public static void main(String[] args) {
+        new Game();
+    }
+}
